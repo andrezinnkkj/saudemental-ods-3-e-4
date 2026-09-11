6420daa26ff3c29008180c420e00960a410835a9
andrezinnkkj <andresilbarr11@gmail.com>
2026-09-10 23:52:30 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,38 +1,26 @@
 const botaoMenu = document.querySelector('.menu-mobile');
 const menu = document.querySelector('#menu-principal');
-
-if (botaoMenu && menu) {
-
-
 const links = menu.querySelectorAll('a');
 
 botaoMenu.addEventListener('click', () => {
-
-    const menuAberto = menu.classList.toggle('menu-aberto');
-
+    menu.classList.toggle('menu-aberto');
     botaoMenu.classList.toggle('menu-aberto');
 
-    botaoMenu.setAttribute('aria-expanded', menuAberto);
+    const aberto = botaoMenu.classList.contains('menu-aberto');
 
+    botaoMenu.setAttribute('aria-expanded', aberto);
     botaoMenu.setAttribute(
         'aria-label',
-        menuAberto ? 'Fechar menu' : 'Abrir menu'
+        aberto ? 'Fechar menu' : 'Abrir menu'
     );
-
 });
 
 links.forEach(link => {
-
     link.addEventListener('click', () => {
-
         menu.classList.remove('menu-aberto');
         botaoMenu.classList.remove('menu-aberto');
 
         botaoMenu.setAttribute('aria-expanded', 'false');
         botaoMenu.setAttribute('aria-label', 'Abrir menu');
-
     });
-
 });
-
-}
